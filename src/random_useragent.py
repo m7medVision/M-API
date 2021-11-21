d16314a321f83ca8af00a49e384fad7f29fdd50c
MAJHCC <info@majhcc.xyz>
2021-11-21 16:43:05 +0400

    remove useragen file and replace it by one online

--- src/random_useragent.py
+++ src/random_useragent.py
@@ -1,6 +1,9 @@
 # open user-agents.txt and return a random user-agent
 import random
+import requests
 def random_useragent():
-    with open(r'src\files\user-agent.txt', 'r') as f:
-        user_agents = f.read().split('\n')
+    f =  requests.get('https://gist.githubusercontent.com/majhcc/4ff967b2f15d01feb0e496eef1db4458/raw/bbed5c3cf42e49643c445d395148c0ea5e06c0f0/user-agent.list').text
+    user_agents = f.split('\n')
     return user_agents[random.randint(0, len(user_agents) - 1)]
+
+# print(random_useragent())
