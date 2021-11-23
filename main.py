700fa9328daab0031dcdaaa0039deba20bca92bd
MAJHCC <info@majhcc.xyz>
2021-11-23 16:18:47 +0400

    FIX SOME BUGS

--- main.py
+++ main.py
@@ -36,12 +36,12 @@ async def Twitter_v2(url: str):
     from src.tweet import get_url_download
     return get_url_download(url)
 
-@app.get("/api/btc")
+@app.get("/api/BTC")
 async def btc():
     from API.BTC import get_btc_price
     return get_btc_price()
 
-@app.get("/api/eth")
+@app.get("/api/ETH")
 async def eth():
     from API.ETH import get_eth_price
     return get_eth_price()
