f1e99707ffef512abe90732dd89c699f76400d22
Mohammed <88824957+majhcc@users.noreply.github.com>
2021-12-27 15:08:20 +0400

    Some changes

--- src/fake_add.py
+++ src/fake_add.py
@@ -1,10 +1,5 @@
 # function that bring fake address from https://www.fakeaddressgenerator.com/
-from itsdangerous import json
 import requests
-from bs4 import BeautifulSoup
-import random
-import string
-import re
 def fake_add():
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29',
