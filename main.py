596f393e5c7417344b705ef46c56e4c1f7aadf4c
Mohammed <88824957+majhcc@users.noreply.github.com>
2021-12-28 21:00:59 +0400

    ~

--- main.py
+++ main.py
@@ -382,4 +382,4 @@ def favicon():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, port=8011)
+    uvicorn.run(app, port=8011, debug=True)
--- src/YouTube_tools.py
+++ src/YouTube_tools.py
@@ -15,8 +15,8 @@ def get_last_video_id_form_c(cid):
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
-        'dnt': '1',
         'upgrade-insecure-requests': '1',
+        'dnt': '1',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'service-worker-navigation-preload': 'true',
@@ -24,16 +24,23 @@ def get_last_video_id_form_c(cid):
         'sec-fetch-mode': 'navigate',
         'sec-fetch-user': '?1',
         'sec-fetch-dest': 'document',
+        'referer': 'https://www.youtube.com/',
         'accept-language': 'en-US,en;q=0.9',
-        'cookie': 'GPS=1; YSC=ld3Ddx9mGaU; VISITOR_INFO1_LIVE=T0u5UGKumko; CONSISTENCY=AGDxDeNCVGYrAVEB2FSVf9eeRZcSvRwOtPLBOFjvp-KJEK5W1WjZW1gYb_uCL6NF1a-IIyaJ3_69-uLJZnaol2xGiIIDlKvwoAVUt3gCQo-DfH1oLq_PvfLmjewSh6pfxD4ZI1AiLDxpJtttcDpxMQ; PREF=tz=Asia.Dubai&f6=40000000',
+        'cookie': 'YSC=FV4EgoS5wJE; PREF=tz=Asia.Dubai; CONSENT=YES+yt.417698365.en+FX+925; VISITOR_INFO1_LIVE=usNfcsw0AHg; GPS=1',
+    }
+    cookies= {
+        'YSC': 'FV4EgoS5wJE',
+        'PREF': 'tz=Asia.Dubai',
+        'CONSENT': 'YES+yt.417698365.en+FX+925',
+        'VISITOR_INFO1_LIVE': 'usNfcsw0AHg',
+        'GPS': '1',
     }
     cid        = str(cid)
-    html       = requests.get('https://www.youtube.com/channel/{}/videos'.format(cid), headers=headers).text
+    html       = requests.get('https://www.youtube.com/channel/{}/videos'.format(cid), headers=headers, cookies=cookies).text
     soup       = BeautifulSoup(html, 'lxml')
     script     = soup.find_all("script")[31]
     m          = re.search('var ytInitialData = (.+)[,;]{1}', str(script)).group(1)
     json_m     = json.loads(m)
     last_video = json_m['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['gridRenderer']['items'][0]['gridVideoRenderer']['videoId']
-   
    
     return last_video
