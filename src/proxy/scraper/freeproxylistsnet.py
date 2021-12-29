1cfff388627a4d1165db7aebf609ca77668e62ab
Mohammed <88824957+majhcc@users.noreply.github.com>
2021-12-29 19:15:29 +0400

    ~

--- src/proxy/scraper/freeproxylistsnet.py
+++ src/proxy/scraper/freeproxylistsnet.py
@@ -3,7 +3,7 @@ import lxml.html
 def get_list():
     alist = []
     count = 0
-    for i in range(1, 11):
+    for i in range(1, 5):
             url = 'https://proxylist.geonode.com/api/proxy-list?limit=50&page={}&sort_by=lastChecked'.format(i)
             response = requests.get(url, timeout=100, headers={'User-Agent': 'Mozilla/5.0'})
             data = response.json()['data']
