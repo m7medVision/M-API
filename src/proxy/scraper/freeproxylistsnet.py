24c1c30a3c6b481979f5551929be4696447f543f
Mohammed <88824957+majhcc@users.noreply.github.com>
2021-12-29 19:12:40 +0400

    ~

--- src/proxy/scraper/freeproxylistsnet.py
+++ src/proxy/scraper/freeproxylistsnet.py
@@ -4,8 +4,8 @@ def get_list():
     alist = []
     count = 0
     for i in range(1, 11):
-            url = 'https://proxylist.geonode.com/api/proxy-list?limit=50000&page={}&sort_by=lastChecked'.format(i)
-            response = requests.get(url)
+            url = 'https://proxylist.geonode.com/api/proxy-list?limit=50&page={}&sort_by=lastChecked'.format(i)
+            response = requests.get(url, timeout=100, headers={'User-Agent': 'Mozilla/5.0'})
             data = response.json()['data']
             for item in data:
                 proxy = item['ip']
